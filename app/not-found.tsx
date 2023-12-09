60a777924d2218f1d1096df3cdfdce3baed80b75
Shivam06-form <shivambhagwat06071998@gmail.com>
2023-12-09 17:45:46 +0530

    UPDATED-10

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        color: "red", alignItems: "center", height: "100vh",
+        justifyContent: "center",
+        // backgroundColor: "GrayText",
+
+      }}
+    ><h1>404 Hum pai to hai he 9</h1></div>
+  )
+}
+
+export default NotFound
